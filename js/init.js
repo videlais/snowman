6bb1f3c76431c6e469bf92e6ffedcb6556d54f2b
Chris Klimas <chris@twofoldsecret.com>
2014-03-23 00:45:08 -0400

    Links work

--- js/init.js
+++ js/init.js
@@ -1,4 +1,9 @@
 $(document).ready(function()
 {
 	window.story = new Story($('*[data-role="twinestory"]'));
+
+	$('body').on('click', 'a[data-passage]', function()
+	{
+		window.story.show($(this).attr('data-passage'));
+	});
 });
--- js/passage.js
+++ js/passage.js
@@ -7,5 +7,27 @@ function Passage (id, name, source)
 
 Passage.prototype.render = function (el)
 {
-	return window.marked(this.source);
+	var rendered = window.marked(this.source);
+
+	// [[displayed text|target]] links
+
+	rendered = rendered.replace(/\[\[(.+?)\|(.+?)\]\]/g,
+	                            '<a href="javascript:void(0)" data-passage="$2">$1</a>');
+
+	// [[displayed text->target]] links
+
+	rendered = rendered.replace(/\[\[(.+?)-&gt;(.+?)\]\]/g,
+	                            '<a href="javascript:void(0)" data-passage="$2">$1</a>');
+
+	// [[target<-displayed text]] links
+
+	rendered = rendered.replace(/\[\[(.+?)&lt;-(.+?)\]\]/g,
+	                            '<a href="javascript:void(0)" data-passage="$1">$2</a>');
+
+	// [[target]] links
+
+	rendered = rendered.replace(/\[\[(.+?)\]\]/g,
+	                            '<a href="javascript:void(0)" data-passage="$1">$1</a>');
+
+	return rendered;
 };
