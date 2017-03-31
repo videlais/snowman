642c98644192b4011e41868d66ee34957a478a32
Chris Klimas <chris@twinery.org>
2017-03-31 18:04:55 -0400

    Add test coverage report, more tests for Passage class

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration
 // Generated on Thu Mar 30 2017 23:19:50 GMT-0400 (EDT)
 
+var istanbul = require('browserify-istanbul');
+
 module.exports = function(config) {
   config.set({
 
@@ -15,7 +17,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'src/**/*.spec.js'
+      'src/**/*.spec.js',
+      'src/**/*!(spec).js'
     ],
 
 
@@ -27,14 +30,15 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'src/**/*.spec.js': 'browserify'
+      'src/**/*.spec.js': 'browserify',
+      'src/**/*!(spec).js': ['browserify']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
@@ -65,6 +69,12 @@ module.exports = function(config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: Infinity,
+
+    browserify: {
+      transform: [istanbul({ ignore: ['**/*.spec.js'] })]
+    },
+
+    coverageReporter: { dir: 'test-coverage' }
   })
 }
--- src/passage.spec.js
+++ src/passage.spec.js
@@ -1,6 +1,19 @@
 var Passage = require('./passage');
+var $ = require('jquery');
 
 describe('Passage', function() {
+	beforeEach(function() {
+		if (!window.story) {
+			window.story = { fake: true, state: {} };
+		}
+	});
+
+	afterEach(function() {
+		if (window.story.fake) {
+			window.story = undefined;
+		}
+	});
+
 	it('offers a static render function', function() {
 		expect(typeof Passage.render).toBe('function');
 	});
@@ -10,29 +23,95 @@ describe('Passage', function() {
 		expect(p.id).toBe(123);
 	});
 
-	it('sets a name with its constructor', function () {
+	it('sets a name with its constructor', function() {
 		var p = new Passage(null, 'The Quick Brown Fox');
 		expect(p.name).toBe('The Quick Brown Fox');
 	});
 
-	it('sets tags with its constructor', function () {
+	it('sets tags with its constructor', function() {
 		var p = new Passage(null, null, ['red', 'blue']);
 		expect(p.tags.length).toBe(2);
 		expect(p.tags[0]).toBe('red');
 		expect(p.tags[1]).toBe('blue');
 	});
 
-	it('sets source with its constructor', function () {
+	it('sets source with its constructor', function() {
 		var p = new Passage(null, null, null, 'Hello world');
 		expect(p.source).toBe('Hello world');
 	});
 
 	it('renders Markdown', function() {
-		var p = new Passage();
-		p.source = 'Hello _there_.';
+		expect(Passage.render('Hello _there_.'))
+			.toBe('<p>Hello <em>there</em>.</p>\n');
+	});
 
-		/* Needs to be stubbed. */
-		window.story = { state: {} };
-		expect(p.render()).toBe('<p>Hello <em>there</em>.</p>\n');
+	it('renders [[basic links]]', function() {
+		var $out = $(Passage.render('[[A link]]'));
+		var $link = $out.find('a');
+
+		expect($link.length).toBe(1);
+		expect($link.eq(0).data('passage')).toBe('A link');
+		expect($link.eq(0).text()).toBe('A link');
+	});
+
+	it('renders [[pipe|links]]', function() {
+		var $out = $(Passage.render('[[Displayed|A link]]'));
+		var $link = $out.find('a');
+
+		expect($link.length).toBe(1);
+		expect($link.eq(0).data('passage')).toBe('A link');
+		expect($link.eq(0).text()).toBe('Displayed');
+	});
+
+	it('renders [[links|to URLs]]', function() {
+		var $out = $(Passage.render('[[Displayed|https://twinery.org]]'));
+		var $link = $out.find('a');
+
+		expect($link.length).toBe(1);
+		expect($link.eq(0).attr('href')).toBe('https://twinery.org');
+		expect($link.eq(0).data('passage')).toBe(undefined);
+		expect($link.eq(0).text()).toBe('Displayed');
+	});
+
+	it('renders [[arrow->links]]', function() {
+		var $out = $(Passage.render('[[Displayed->A link]]'));
+		var $link = $out.find('a');
+
+		expect($link.length).toBe(1);
+		expect($link.eq(0).data('passage')).toBe('A link');
+		expect($link.eq(0).text()).toBe('Displayed');
+	});
+
+	it('renders [[links->to URLs]]', function() {
+		var $out = $(Passage.render('[[Displayed->https://twinery.org]]'));
+		var $link = $out.find('a');
+
+		expect($link.length).toBe(1);
+		expect($link.eq(0).attr('href')).toBe('https://twinery.org');
+		expect($link.eq(0).data('passage')).toBe(undefined);
+		expect($link.eq(0).text()).toBe('Displayed');
+	});
+
+	it('renders [[arrow<-links]]', function() {
+		var $out = $(Passage.render('[[A link<-Displayed]]'));
+		var $link = $out.find('a');
+
+		expect($link.length).toBe(1);
+		expect($link.eq(0).data('passage')).toBe('A link');
+		expect($link.eq(0).text()).toBe('Displayed');
+	});
+
+	it('renders [[URL<-links]]', function() {
+		var $out = $(Passage.render('[[https://twinery.org<-Displayed]]'));
+		var $link = $out.find('a');
+
+		expect($link.length).toBe(1);
+		expect($link.eq(0).attr('href')).toBe('https://twinery.org');
+		expect($link.eq(0).data('passage')).toBe(undefined);
+		expect($link.eq(0).text()).toBe('Displayed');
+	});
+
+	it('renders EJS tags', function() {
+		expect(Passage.render('<% print("Hello") %>')).toBe('<p>Hello</p>\n');
 	});
 });
