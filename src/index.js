66696e5b0ed97320a04a36684433b0858f01bc10
Dan Cox <dan.cox@videlais.com>
2019-07-23 06:36:23 -0400

    Addition of link(), random(), and randomNumber()
    Addition of window.link(), window.random(), and window.randomNumber()
    based on the function definitions found in the story format Protagonist.

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ var _ = window._ = require('underscore');
 var marked = window.marked = require('marked');
 var Story = window.Story = require('./story');
 var Passage = window.Passage = require('./passage');
+var Window = require('./window');
 /* eslint-enable no-unused-vars */
 
 $(function() {
 	window.story = new Story($('tw-storydata'));
+	// Bind functions
+	var binding = new Window();
 	window.story.start($('#main'));
 });
