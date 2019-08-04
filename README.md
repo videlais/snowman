819d66f47175c63f74dad91e0df3488c6e2ce4d1
Dan Cox <dan.cox@videlais.com>
2019-08-04 14:02:24 -0400

    Re-upload of Snowman 2.0

---
+++ README.md
@@ -0,0 +1,25 @@
+# Snowman 2.0.0
+
+Snowman 2.0.0 is a minimal Twine 2 story format designed for people who already know
+JavaScript and CSS originally created by Chris Klimas. It is current maintained by Dan Cox.
+
+## What does Snowman offer?
+
+Snowman does not use macros like Harlowe, SugarCube, and other story formats. Instead, it has two global objects, `window.story` and `window.passage`, that expose functionality for working with the overall story and the current passage, respectively.
+
+It also includes the JavaScript libraries [jQuery](https://jquery.com/) and [Underscore](https://underscorejs.org/).
+
+Additional styling options are supplied through the inclusion of the [Marked](https://github.com/markedjs/marked) library.
+
+## Want to learn more?
+
+The [official Snowman documentation](https://videlais.github.io/snowman/) has more details about Snowman including multiple examples of how to do various tasks.
+
+## Building
+
+Run `npm install` to install dependencies.
+
+`npm run build` will create a Twine 2-ready story format under `dist/`.
+
+To check for style errors, run `npm run lint`.
+To run unit tests, run `npm test`.
---
+++ scripts/build-format.js
@@ -0,0 +1,49 @@
+var ejs = require('ejs');
+var exec = require('child-process-promise').exec;
+var fs = require('fs');
+var pkg = require('../package.json');
+var shell = require('shelljs');
+let CleanCSS = require('clean-css');
+
+var encoding = { encoding: 'utf8' };
+
+function buildCSS() {
+
+	shell.rm('-f', 'lib/src/format.css');
+	shell.cat('lib/src/*.css').to('lib/src/format.css');
+  let file = fs.readFileSync('lib/src/format.css');
+  const output = new CleanCSS({level: 2}).minify(file);
+	return output.styles;
+
+}
+
+Promise.all([
+	buildCSS(),
+	exec('browserify -g uglifyify lib/index.js -t [ babelify --presets [ @babel/env ] ]', { maxBuffer: Infinity })
+]).then(function(results) {
+	var distPath = 'dist/' + pkg.name.toLowerCase() + '-' + pkg.version;
+	var htmlTemplate = ejs.compile(fs.readFileSync('lib/src/index.ejs', encoding));
+	var formatData = {
+		author: pkg.author.replace(/ <.*>/, ''),
+		description: pkg.description,
+		image: 'icon.svg',
+		name: pkg.name,
+		proofing: false,
+		source: htmlTemplate({
+			style: results[0],
+			script: results[1].stdout
+		}),
+		url: pkg.repository,
+		version: pkg.version
+	};
+
+	shell.mkdir('-p', distPath);
+
+	fs.writeFileSync(
+		distPath + '/format.js',
+		'window.storyFormat(' + JSON.stringify(formatData) + ');'
+	);
+
+	shell.cp('lib/src/icon.svg', distPath + '/icon.svg');
+
+});
