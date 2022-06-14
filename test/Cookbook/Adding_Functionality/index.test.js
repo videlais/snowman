c56e5135ed788cf7a09862a8f37952c5a1fb5ea1
Dan Cox <daniel.57@gmail.com>
2022-06-14 08:59:06 -0400

    Testing: Cookbook: Conditional Statements

--- test/Cookbook/Adding_Functionality/index.test.js
+++ test/Cookbook/Adding_Functionality/index.test.js
@@ -6,14 +6,15 @@ const path = require('path');
 require('expect-puppeteer');
 
 // Create the index.html file to test
-ShellJS.exec("extwee -c -s dist/snowman-2.2.0-format.js -i test/Cookbook/AddingFunctionality/snowman_adding_functionality.twee -o test/Cookbook/AddingFunctionality/index.html");
-
+ShellJS.exec(`extwee -c -s dist/snowman-2.2.0-format.js -i ${path.join(__dirname, 'index.twee')} -o ${path.join(__dirname, 'index.html')}`);
+ 
 describe('Cookbook - Adding Functionality', () => {
   beforeAll(async () => {
     await page.goto(`file://${path.join(__dirname, 'index.html')}`);
   });
 
-  it('Should display "The current time is" on page', async () => {
-    await expect(page).toMatch('The current time is');
+  it('Should display current year on page', async () => {
+    const year = new Date().getFullYear().toString();
+    await expect(page).toMatch(year);
   });
-})
+});
---
+++ test/Cookbook/Conditional_Statements/index.test.js
@@ -0,0 +1,19 @@
+/**
+ * @jest-environment puppeteer
+ */
+ const ShellJS = require('shelljs');
+ const path = require('path');
+ require('expect-puppeteer');
+ 
+ // Create the index.html file to test
+ ShellJS.exec(`extwee -c -s dist/snowman-2.2.0-format.js -i ${path.join(__dirname, 'index.twee')} -o ${path.join(__dirname, 'index.html')}`);
+ 
+ describe('Cookbook - Conditional Statements', () => {
+   beforeAll(async () => {
+     await page.goto(`file://${path.join(__dirname, 'index.html')}`);
+   });
+ 
+   it(`Should display "It's a horse!"`, async () => {
+     await expect(page).toMatch("It's a horse!");
+   });
+ });
