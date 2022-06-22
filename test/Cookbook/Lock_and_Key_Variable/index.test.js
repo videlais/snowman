7889a6e41c1d088d47faebc5716506b8af9f4719
Dan Cox <daniel.57@gmail.com>
2022-06-22 11:54:02 -0400

    Testing: Cookbook: Lock and Key (Variable)

---
+++ test/Cookbook/Lock_and_Key_Variable/index.test.js
@@ -0,0 +1,26 @@
+/**
+ * @jest-environment puppeteer
+ */
+const ShellJS = require('shelljs');
+const path = require('path');
+require('expect-puppeteer');
+ 
+ShellJS.exec(`extwee -c -s dist/snowman-2.2.0-format.js -i ${path.join(__dirname, 'index.twee')} -o ${path.join(__dirname, 'index.html')}`);
+  
+describe('Cookbook - Lock and Key: Variable', () => {
+   beforeAll(async () => {
+     await page.goto(`file://${path.join(__dirname, 'index.html')}`);
+   });
+
+   afterAll(async () => {
+    ShellJS.rm(`${path.join(__dirname, 'index.html')}`);
+   });
+ 
+    it('Should display result', async () => {
+        await page.click('[data-passage="Back Room"]');
+        await page.click('[class="key-item"]');
+        await page.click('[data-passage="Front Room"]');
+        await page.click('[data-passage="Exit"]');
+        await expect(page).toMatch('You found the key and went through the door!');
+   });
+ });
