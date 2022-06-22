84bc3d0148af1a458245d6555e892aa5cd75e8aa
Dan Cox <daniel.57@gmail.com>
2022-06-22 11:41:38 -0400

    Testing: Cookbook: Story and Passage API

---
+++ test/Cookbook/Story_and_Passage_API/index.test.js
@@ -0,0 +1,22 @@
+/**
+ * @jest-environment puppeteer
+ */
+const ShellJS = require('shelljs');
+const path = require('path');
+require('expect-puppeteer');
+ 
+ShellJS.exec(`extwee -c -s dist/snowman-2.2.0-format.js -i ${path.join(__dirname, 'index.twee')} -o ${path.join(__dirname, 'index.html')}`);
+  
+describe('Cookbook - Story and Passage API', () => {
+   beforeAll(async () => {
+     await page.goto(`file://${path.join(__dirname, 'index.html')}`);
+   });
+
+   afterAll(async () => {
+    ShellJS.rm(`${path.join(__dirname, 'index.html')}`);
+   });
+ 
+    it('Should display storage passage content', async () => {
+        await expect(page).toMatch('This is content in the storage passage!');
+   });
+ });
