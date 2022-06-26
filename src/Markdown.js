e6f8701d38ef3a8723a1a9217b20dda2a9549ef3
Dan Cox <daniel.57@gmail.com>
2022-06-26 12:14:36 -0400

    Removing most Markdown support

--- src/Markdown.js
+++ src/Markdown.js
@@ -8,34 +8,7 @@ class Markdown {
       // [[dest<-rename]]
       [/\[\[(.*?)<-(.*?)\]\]/g, '<tw-link role="link" data-passage="$1">$2</a>'],
       // [[destination]]
-      [/\[\[(.*?)\]\]/g, '<tw-link role="link" data-passage="$1">$1</a>'],
-      // ##### Heading level 5
-      [/#{5}\s?([^\n]+)/g, '<h5>$1</h5>'],
-      // #### Heading level 4
-      [/#{4}\s?([^\n]+)/g, '<h4>$1</h4>'],
-      // ### Heading level 3
-      [/#{3}\s?([^\n]+)/g, '<h3>$1</h3>'],
-      // ## Heading level 2
-      [/#{2}\s?([^\n]+)/g, '<h2>$1</h2>'],
-      // # Heading level 1
-      [/#{1}\s?([^\n]+)/g, '<h1>$1</h1>'],
-      // **bold text**
-      [/\*\*\s?([^\n]+)\*\*/g, '<strong>$1</strong>'],
-      // __bold text__
-      [/__([^_]+)__/g, '<strong>$1</strong>'],
-      // *italic text*
-      [/\*\s?([^\n]+)\*/g, '<em>$1</em>'],
-      // _italic text_
-      [/_([^_`]+)_/g, '<em>$1</em>'],
-      // Line Item (*/+/-)
-      [/\n([\*|\-|\+])(.*)/g, '<ul><li>$2</li></ul>'], // eslint-disable-line
-      // Horizontal Line
-      [/(=|-){3}/g, '<hr>'],
-      // Image
-      [
-        /!\[([^\]]+)\]\(([^)]+)\s"([^")]+)"\)/g,
-        '<img src="$2" alt="$1" title="$3" />'
-      ]
+      [/\[\[(.*?)\]\]/g, '<tw-link role="link" data-passage="$1">$1</a>']
     ];
 
     rules.forEach(([rule, template]) => {
--- test/Markdown.test.js
+++ test/Markdown.test.js
@@ -2,66 +2,6 @@ const Markdown = require('../src/Markdown.js');
 
 describe('Markdown', () => {
   describe('parse()', () => {
-    it('Should parse header level five', () => {
-      expect(Markdown.parse('##### Header')).toBe('<h5>Header</h5>');
-    });
-
-    it('Should parse header level four', () => {
-      expect(Markdown.parse('#### Header')).toBe('<h4>Header</h4>');
-    });
-
-    it('Should parse header level three', () => {
-      expect(Markdown.parse('### Header')).toBe('<h3>Header</h3>');
-    });
-
-    it('Should parse header level two', () => {
-      expect(Markdown.parse('## Header')).toBe('<h2>Header</h2>');
-    });
-
-    it('Should parse header level one', () => {
-      expect(Markdown.parse('# Header')).toBe('<h1>Header</h1>');
-    });
-
-    it('Should produce bold - asterisks', () => {
-      expect(Markdown.parse('**bold text**')).toBe('<strong>bold text</strong>');
-    });
-
-    it('Should produce bold - underscores', () => {
-      expect(Markdown.parse('__bold text__')).toBe('<strong>bold text</strong>');
-    });
-
-    it('Should produce emphasis - underscore', () => {
-      expect(Markdown.parse('_text_')).toBe('<em>text</em>');
-    });
-
-    it('Should produce emphasis - asterisk', () => {
-      expect(Markdown.parse('*text*')).toBe('<em>text</em>');
-    });
-
-    it('Should produce one line item - plus', () => {
-      expect(Markdown.parse('\n+text')).toBe('<ul><li>text</li></ul>');
-    });
-
-    it('Should produce one line item - minus', () => {
-      expect(Markdown.parse('\n-text')).toBe('<ul><li>text</li></ul>');
-    });
-
-    it('Should produce one line item - asterisk', () => {
-      expect(Markdown.parse('\n*text')).toBe('<ul><li>text</li></ul>');
-    });
-
-    it('Should produce multiple line items - plus', () => {
-      expect(Markdown.parse('\n+text\n+text')).toBe('<ul><li>text</li></ul><ul><li>text</li></ul>');
-    });
-
-    it('Should produce <hr> - hyphen', () => {
-      expect(Markdown.parse('---')).toBe('<hr>');
-    });
-
-    it('Should produce <hr> - equal sign', () => {
-      expect(Markdown.parse('===')).toBe('<hr>');
-    });
-
     it('Should produce classic link', () => {
       expect(Markdown.parse('[[dest]]')).toBe('<tw-link role="link" data-passage="dest">dest</a>');
     });
