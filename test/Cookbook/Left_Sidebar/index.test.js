34f96568a8a9a69342e715b7afac8ad7d040fc66
Dan Cox <daniel.57@gmail.com>
2022-06-26 14:39:19 -0400

    Testing: Cookbook: Left Sidebar

---
+++ test/Cookbook/Left_Sidebar/index.test.js
@@ -0,0 +1,22 @@
+/**
+ * @jest-environment puppeteer
+ */
+const ShellJS = require('shelljs');
+const path = require('path');
+require('expect-puppeteer');
+ 
+ShellJS.exec(`extwee -c -s dist/snowman-2.2.0-format.js -i ${path.join(__dirname, 'index.twee')} -o ${path.join(__dirname, 'index.html')}`);
+   
+describe('Cookbook - Left Sidebar', () => {
+    beforeAll(async () => {
+        await page.goto(`file://${path.join(__dirname, 'index.html')}`);
+    });
+ 
+    afterAll(async () => {
+        ShellJS.rm(`${path.join(__dirname, 'index.html')}`);
+    });
+  
+    it('Should display "Name: Jane Doe" after story start', async () => {
+        await expect(page).toMatch('Name: Jane Doe');
+    });
+ });
