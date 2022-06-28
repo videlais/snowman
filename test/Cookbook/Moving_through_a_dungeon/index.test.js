1afbf23b2d20befd40c6d69f1df7cf7350aee459
Dan Cox <daniel.57@gmail.com>
2022-06-28 14:14:36 -0400

    Testing: Cookbook: Moving through a dungeon

---
+++ test/Cookbook/Moving_through_a_dungeon/index.test.js
@@ -0,0 +1,23 @@
+/**
+ * @jest-environment puppeteer
+ */
+const ShellJS = require('shelljs');
+const path = require('path');
+require('expect-puppeteer');
+ 
+ShellJS.exec(`extwee -c -s dist/snowman-2.2.0-format.js -i ${path.join(__dirname, 'index.twee')} -o ${path.join(__dirname, 'index.html')}`);
+   
+describe('Cookbook - Moving through a dungeon', () => {
+    beforeAll(async () => {
+        await page.goto(`file://${path.join(__dirname, 'index.html')}`);
+    });
+ 
+    afterAll(async () => {
+        ShellJS.rm(`${path.join(__dirname, 'index.html')}`);
+    });
+  
+    it('Should display "#.P.#.....#"', async () => {
+        await page.click('[data-move="e"]');
+        await expect(page).toMatch("#.P.#.....#");
+    });
+});
