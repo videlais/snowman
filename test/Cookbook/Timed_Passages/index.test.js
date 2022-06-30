2ebf973311a4679b765eb5715dc9562f2ca9b153
Dan Cox <daniel.57@gmail.com>
2022-06-30 19:54:17 -0400

    Testing: Cookbook: Timed Passages

---
+++ test/Cookbook/Timed_Passages/index.test.js
@@ -0,0 +1,27 @@
+/**
+ * @jest-environment puppeteer
+ */
+const ShellJS = require('shelljs');
+const path = require('path');
+require('expect-puppeteer');
+
+// 15 seconds
+jest.setTimeout(15000);
+  
+ShellJS.exec(`extwee -c -s dist/snowman-2.2.0-format.js -i ${path.join(__dirname, 'index.twee')} -o ${path.join(__dirname, 'index.html')}`);
+   
+describe('Cookbook - Timed Passages', () => {
+    beforeAll(async () => {
+        await page.goto(`file://${path.join(__dirname, 'index.html')}`);
+    });
+ 
+    afterAll(async () => {
+        ShellJS.rm(`${path.join(__dirname, 'index.html')}`);
+    });
+  
+    it('Should display "world ended"', async () => {
+        // Wait for 11 seconds
+        await page.waitForTimeout(11000);
+        await expect(page).toMatch('The world ended.');
+    });
+});
