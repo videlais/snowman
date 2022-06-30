3f12cd58efbbe27a398518526d2cb38460f3074d
Dan Cox <daniel.57@gmail.com>
2022-06-30 15:23:02 -0400

    Testing: Cookbook: Static Healthbars

--- test/Cookbook/Static_Healthbars/index.test.js
+++ test/Cookbook/Static_Healthbars/index.test.js
@@ -4,19 +4,22 @@
 const ShellJS = require('shelljs');
 const path = require('path');
 require('expect-puppeteer');
-  
+ 
 ShellJS.exec(`extwee -c -s dist/snowman-2.2.0-format.js -i ${path.join(__dirname, 'index.twee')} -o ${path.join(__dirname, 'index.html')}`);
-    
-describe('Cookbook - Render Passage to Element', () => {
+  
+describe('Cookbook - Static Healthbars', () => {
     beforeAll(async () => {
         await page.goto(`file://${path.join(__dirname, 'index.html')}`);
     });
-  
+
     afterAll(async () => {
         ShellJS.rm(`${path.join(__dirname, 'index.html')}`);
     });
-   
-    it('Should display "This is the HUD!"', async () => {
-        await expect(page).toMatch('This is the HUD!');
+ 
+    it('Should display meter', async () => {
+        // Wait for the <meter>.
+        await page.waitForSelector('meter');
+        // Check for the text before it.
+        await expect(page).toMatch('Show a healthbar using a Meter element:');
     });
 });
