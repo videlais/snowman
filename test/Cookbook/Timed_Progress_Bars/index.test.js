ebcecb463611948fe97ebf8602c00268b4ff74af
Dan Cox <daniel.57@gmail.com>
2022-06-30 20:52:18 -0400

    Testing: Cookbook: Timed Progress Bars

---
+++ test/Cookbook/Timed_Progress_Bars/index.test.js
@@ -0,0 +1,27 @@
+/**
+ * @jest-environment puppeteer
+ */
+const ShellJS = require('shelljs');
+const path = require('path');
+require('expect-puppeteer');
+ 
+// 10 seconds
+jest.setTimeout(10000);
+   
+ShellJS.exec(`extwee -c -s dist/snowman-2.2.0-format.js -i ${path.join(__dirname, 'index.twee')} -o ${path.join(__dirname, 'index.html')}`);
+    
+describe('Cookbook - Timed Progress Bars', () => {
+    beforeAll(async () => {
+        await page.goto(`file://${path.join(__dirname, 'index.html')}`);
+    });
+  
+    afterAll(async () => {
+        ShellJS.rm(`${path.join(__dirname, 'index.html')}`);
+    });
+   
+    it('Should display "too late"', async () => {
+        // Wait for 7 seconds
+        await page.waitForTimeout(7000);
+        await expect(page).toMatch('Too late!');
+    });
+});
