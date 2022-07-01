ca1905e9c0838b24581b815a3adbab1f9661e32c
Dan Cox <daniel.57@gmail.com>
2022-06-30 21:05:43 -0400

    Testing: Cookbook: Typewriter Effect

---
+++ test/Cookbook/Typewriter_Effect/index.test.js
@@ -0,0 +1,27 @@
+/**
+ * @jest-environment puppeteer
+ */
+const ShellJS = require('shelljs');
+const path = require('path');
+require('expect-puppeteer');
+ 
+// 15 seconds
+jest.setTimeout(15000);
+   
+ShellJS.exec(`extwee -c -s dist/snowman-2.2.0-format.js -i ${path.join(__dirname, 'index.twee')} -o ${path.join(__dirname, 'index.html')}`);
+    
+describe('Cookbook - Typewriter Effect', () => {
+    beforeAll(async () => {
+        await page.goto(`file://${path.join(__dirname, 'index.html')}`);
+    });
+  
+    afterAll(async () => {
+        ShellJS.rm(`${path.join(__dirname, 'index.html')}`);
+    });
+   
+    it('Should display "Hello, world!"', async () => {
+        // Wait for 12 seconds
+        await page.waitForTimeout(12000);
+        await expect(page).toMatch('Hello, world!');
+    });
+});
