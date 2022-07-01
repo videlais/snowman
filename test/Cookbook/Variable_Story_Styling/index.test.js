10757421c828bdc2d69f85ebd45fb205fe246817
Dan Cox <daniel.57@gmail.com>
2022-06-30 21:27:59 -0400

    Testing: Cookbook: Variable Story Styling

---
+++ test/Cookbook/Variable_Story_Styling/index.test.js
@@ -0,0 +1,23 @@
+/**
+ * @jest-environment puppeteer
+ */
+const ShellJS = require('shelljs');
+const path = require('path');
+require('expect-puppeteer');
+ 
+ShellJS.exec(`extwee -c -s dist/snowman-2.2.0-format.js -i ${path.join(__dirname, 'index.twee')} -o ${path.join(__dirname, 'index.html')}`);
+   
+describe('Cookbook - Variable Story Styling', () => {
+    beforeAll(async () => {
+        await page.goto(`file://${path.join(__dirname, 'index.html')}`);
+    });
+ 
+    afterAll(async () => {
+        //ShellJS.rm(`${path.join(__dirname, 'index.html')}`);
+    });
+  
+    it('Should display "This text is white on a black background."', async () => {
+        await page.click('[data-passage="Next Passage"]');
+        await expect(page).toMatch("This text is white on a black background.");
+    });
+});
