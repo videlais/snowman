7a053c8c7a0f3643cf275def9cc9d9f834302bbf
Dan Cox <daniel.57@gmail.com>
2022-07-01 16:19:34 -0400

    Testing: Cookbook: Dropdown

---
+++ test/Cookbook/Dropdown/index.test.js
@@ -0,0 +1,27 @@
+/**
+ * @jest-environment puppeteer
+ */
+const ShellJS = require('shelljs');
+const path = require('path');
+require('expect-puppeteer');
+ 
+ ShellJS.exec(`extwee -c -s dist/snowman-2.2.0-format.js -i ${path.join(__dirname, 'index.twee')} -o ${path.join(__dirname, 'index.html')}`);
+  
+describe('Cookbook - Dropdown', () => {
+    beforeAll(async () => {
+        await page.goto(`file://${path.join(__dirname, 'index.html')}`);
+    });
+
+    afterAll(async () => {
+        ShellJS.rm(`${path.join(__dirname, 'index.html')}`);
+    });
+ 
+    it('Should display up', async () => {
+        // Select an option.
+        await page.select('select#directions', 'up');
+        // Go to next passage.
+        await page.click('[data-passage="Show Direction"]');
+        // Expect to see "up".
+        await expect(page).toMatch('The direction was up.');
+    });
+ });
