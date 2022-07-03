919317e6b19a780ffe9e03e30e71c74c77cb3b6a
Dan Cox <daniel.57@gmail.com>
2022-07-03 18:06:38 -0400

    Testing: RunScript: Remove Passage Test

---
+++ test/RunScript/RemovePassage_Test/index.test.js
@@ -0,0 +1,22 @@
+/**
+ * @jest-environment puppeteer
+ */
+const ShellJS = require('shelljs');
+const path = require('path');
+require('expect-puppeteer');
+   
+ShellJS.exec(`extwee -c -s dist/snowman-2.2.0-format.js -i ${path.join(__dirname, 'index.twee')} -o ${path.join(__dirname, 'index.html')}`);
+    
+describe('RunScript - Remove Passage Test', () => {
+    beforeAll(async () => {
+        await page.goto(`file://${path.join(__dirname, 'index.html')}`);
+    });
+  
+    afterAll(async () => {
+        ShellJS.rm(`${path.join(__dirname, 'index.html')}`);
+    });
+   
+    it('Should display paragraph', async () => {
+        await expect(page).toMatch('Show me!');
+    });
+});
