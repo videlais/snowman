4ba87d07962f8d04baf8f28de84655f3c08839f8
Dan Cox <dan.cox@videlais.com>
2025-11-19 22:12:43 -0600

    Updating builds

--- build-format.js
+++ build-format.js
@@ -1,16 +1,44 @@
 const fs = require('node:fs');
 const ejs = require('ejs');
 
+const files = [
+  'format.js',
+  'icon.svg',
+  'LICENSE'
+];
+
+console.log(`Cleaning dist/`);
+
+files.forEach(file => {
+  const path = `dist/${file}`;
+
+  if (fs.existsSync(path)) {
+    fs.unlinkSync(path);
+    console.log(`\t 🗑️ Deleted ${path}`);
+  }
+});
+
+console.log(`Building format.js file...`);
+
 // Read and parse story.json file.
 const storyFile = fs.readFileSync('story.json', { encoding: 'utf8' });
 const story = JSON.parse(storyFile);
 
 // Read story format HTML template.
 const srcIndex = fs.readFileSync('src/story.ejs', { encoding: 'utf8' });
+
+console.log(`\t ✅Reading story source EJS`);
+
 // Read bundled format.
 const formatSource = fs.readFileSync('build/format.bundle.js', { encoding: 'utf8' });
+
+console.log(`\t ✅Reading format bundle`);
+
 // Read bundled CSS.
 const storyCSS = fs.readFileSync('build/format.css', { encoding: 'utf8' });
+
+console.log(`\t ✅Reading story CSS bundle`);
+
 // Replace the bundles in the format HTML template.
 const indexSource = ejs.render(srcIndex, {
   format: `<script>${formatSource}</script>`,
@@ -42,3 +70,15 @@ story.editorExtensions = {
 let format = `window.storyFormat(${JSON.stringify(story, null, 2)});`;
 
 fs.writeFileSync('dist/format.js', format);
+
+console.log(`\t ✅Writing dist/format.js`);
+
+// Copy LICENSE file
+fs.copyFileSync('LICENSE', 'dist/LICENSE');
+console.log(`\t ✅Copying LICENSE file`);
+
+// Copy icon.svg file
+fs.copyFileSync('icon.svg', 'dist/icon.svg');
+console.log(`\t ✅Copying icon.svg file`);
+
+console.log(`Build complete!`);
